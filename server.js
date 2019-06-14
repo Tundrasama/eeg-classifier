962b080ae5a49b80e8bd376522c458d30d21ceeb
Tundrasama <tundrasama@gmail.com>
2019-06-14 05:50:40 -0500

    before trip to austin

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const connectDB = require('./config/db');
 const classification = require('./models/Classification');
+const path = require('path');
 
 var db;
 
@@ -14,12 +15,14 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(3000, () => {
   console.log('listening on 3000');
 });
 
 // route = '/test.html';
-route = '/public/eeg-classification.html';
+route = '/public/new-eeg-classification.html';
 // route = '/jsontest.html';
 
 // app.use('/api/channel/', require('./routes/classification'));
