841875f6dbb19af8988a44ca8da801c93bccf775
Tundrasama <tundrasama@gmail.com>
2019-06-22 14:00:01 -0500

    still working on heroku

--- config/db.js
+++ config/db.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
+console.log(db.mongoURI);
+
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
