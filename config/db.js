7c75d723ee5cb41f3324846a23e92097e2075ee1
Tundrasama <tundrasama@gmail.com>
2019-06-24 16:31:40 -0500

    mongodb shit

--- config/db.js
+++ config/db.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose');
 const config = require('config');
 let db;
 
-console.log(process.env.NODE_ENV, process.env.MONGO_URI);
+console.log(
+  process.env.MONGOLAB_URI,
+  process.env.NODE_ENV,
+  process.env.MONGO_URI
+);
 
 if (process.env.NODE_ENV == 'production') {
-  db = process.env.MONGO_URI;
+  // db = process.env.MONGO_URI;
+  db = process.env.MONGOLAB_URI;
 } else {
   db = config.get('mongoURI');
 }
